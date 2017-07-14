0f3b0c919b5daa819a9fc59bb4f2ec3cb362817a
Jose Alvarado <jose_alvarado17@hotmail.com>
2017-07-14 04:19:42 -0400

    Add Fork button to Scafford preview

--- app/scaffold/index.jsx
+++ app/scaffold/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 const Scaffold = (props) => (
     <div>
+        <a className="button" href="https://github.com/josealvaradoo/react-scaffold"><span className="fa fa-code-fork"></span> Fork on Github</a>
         <img src="https://webpack.js.org/6bc5d8cf78d442a984e70195db059b69.svg" className="webpack-logo" />
         <div className="title">React Scaffold</div>
         <div className="author"><a href="https://github.com/josealvaradoo">By Jose Alvarado</a></div>
